98675632dc153e2d333f01da7d388fe966206f7f
Ben Allfree <ben@benallfree.com>
2012-08-19 21:29:16 -0700

    Added fetch()

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ The URL mixin for the [Wicked framework](http://github.com/benallfree/wicked.git
 
 ### W::url_sprintf($string, **$arg1**, ...)
 
+### W::url_fetch($url, $headers = array(), $http_user='', $http_pass='',$use_cache=null)
--- UrlMixin.class.php
+++ UrlMixin.class.php
@@ -16,4 +16,38 @@ class UrlMixin extends Mixin
     $url = call_user_func_array('sprintf', $args);
     return $url;
   }
+
+  static function fetch($url, $headers = array(), $http_user='', $http_pass='',$use_cache=null)
+  {
+    $config = W::module('url');
+    if($use_cache===null) $use_cache = $config['use_cache'];
+    $keys = array_merge(array($url, $http_user, $http_pass), array_values($headers));
+    $md5 = md5(join('|',$keys));
+    $fname = $config['cache_fpath']."$md5";
+    if($use_cache && file_exists($fname)) return json_decode(file_get_contents($fname),true);
+    $ch = curl_init();
+    $timeout = 5;
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_VERBOSE, true); // Display communication with server
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return data instead of display to std out
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    if($http_user)
+    {
+      curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC ) ; 
+      curl_setopt($ch, CURLOPT_USERPWD, "{$http_user}:{$http_pass}");
+    }                     
+    $data = curl_exec($ch);
+    $error = curl_error($ch);
+    $info = curl_getinfo($ch);
+    curl_close($ch);
+    $res = array($data, $error, $info);
+    if($use_cache)
+    {
+      file_put_contents($fname,json_encode($res));    
+    } 
+    return $res;
+  }     
 }
